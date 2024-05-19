2f1f4d6588fedb42b3aaa7ae94f4fcf6c3bdc1e6
parthpandyappp <parthpandyappp@gmail.com>
2024-05-19 22:25:36 +0530

    feat: login-signup api/routes

---
+++ app/api/login/route.ts
@@ -0,0 +1,50 @@
+import bcrypt from "bcryptjs";
+const sign = require("jwt-encode");
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  try {
+    const { username, password } = await req.json();
+
+    if (!username || !password) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const foundUser = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!foundUser) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, foundUser.password);
+    console.log(passwordMatch);
+
+    if (passwordMatch) {
+      const encodedToken = sign(
+        { username: foundUser.username, email: foundUser.email },
+        process.env.JWT_SECRET
+      );
+      return NextResponse.json(
+        { message: "Login successful", user: foundUser, encodedToken },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "Invalid username or password" },
+        { status: 401 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to login" }, { status: 500 });
+  }
+}
